b697b7f130d9f0ea04e0c5686b3596f02a8573d5
github username <github email address>
2020-04-22 13:58:24 -0400

    add description

---
+++ lib/QuarantineLiveStreams.rb
@@ -0,0 +1,6 @@
+require "QuarantineLiveStreams/version"
+
+module QuarantineLiveStreams
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/QuarantineLiveStreams/version.rb
@@ -0,0 +1,3 @@
+module QuarantineLiveStreams
+  VERSION = "0.1.0"
+end
