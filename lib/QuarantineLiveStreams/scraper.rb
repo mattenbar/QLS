22876243398414739ef4dabbd85cf5e8265495b2
github username <github email address>
2020-04-24 09:36:23 -0400

    working on getting time

--- lib/QuarantineLiveStreams/scraper.rb
+++ lib/QuarantineLiveStreams/scraper.rb
@@ -8,11 +8,37 @@ class Scraper
         events_array = []
         url = "https://www.npr.org/2020/03/17/816504058/a-list-of-live-virtual-concerts-to-watch-during-the-coronavirus-shutdown"
         doc = Nokogiri::HTML(open(url))
-        doc.css(".edTag").css("h3").each do |tag|
-            events_array << tag.text
-        end
-        binding.pry
-        events_array
-    end
+        content = doc.css(".storytext")
+        
+        found = false
+        events_array = []
 
+        event_obj = {}
+
+        content.children.each do |el|
+            if (el.name == 'h3')
+                found = true
+            end
+            
+            if ((el.name == 'h3' || el.name == 'p' || el.name == 'h6') && found)
+                found = true
+                
+                case el.name                   
+                    when 'h3'
+                        event_obj = {}
+                        event_obj[:date] = el.text
+                    when 'h6'
+                        event_obj[:genre] = el.text
+                    when 'p'
+                        event_obj[:name] = el.css('strong').text
+                        if event_obj[:time] = el.text.include?("Time")
+                            event_obj[:time] = el.css()
+                        end
+                        binding.pry
+                        event_obj[:link] = el.css('href')
+                end
+            end
+        end
+
+    end
 end
